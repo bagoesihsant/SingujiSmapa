478cb85131d8726a93baaed4a8ca88ab560e2d38
Ahamd Daifullah <adefsing@gmail.com>
2020-06-25 21:02:50 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/bagoesihsant/SingujiSmapa""
    This reverts commit 6be9e2427b84a83eb39136261c996815f3f63876.

---
+++ folder_php/home.php
@@ -0,0 +1,19 @@
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == "GET")
+{
+   
+     $con = mysqli_connect('localhost','root','','db_singuji');
+ 
+    $query = "SELECT tb_mapel.mata_pelajaran, tb_guru.nama_guru FROM tb_guru, tb_mapel WHERE tb_guru.id_mapel=tb_mapel.id_mapel"; //select table in database
+    $sql = mysqli_query($con, $query);
+    $arraydata = array();
+    while ($data = mysqli_fetch_array($sql)) {
+        $arraydata[] = $data;
+    }
+ 
+    echo json_encode($arraydata);
+
+ }
+?>
