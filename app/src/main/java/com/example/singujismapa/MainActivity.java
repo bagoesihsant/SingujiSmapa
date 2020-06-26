775e539704b5e02b43e5506a7dc4c1d80593a537
ummalatulkamilah <55658527+ummalatulkamilah@users.noreply.github.com>
2020-06-26 11:35:46 +0700

    Update MainActivity.java
    tambah onclick untuk navdrawer dan togel di navdrawer
    untuk xml navdrawer jadi satu sama comit "tambah homeadapter" ngga sengaja keikut tadi wkkwkw

--- app/src/main/java/com/example/singujismapa/MainActivity.java
+++ app/src/main/java/com/example/singujismapa/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.singujismapa;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,17 +14,40 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     BottomNavigationView btm_view;
+    DrawerLayout dlay;
+    NavigationView nav_view;
+    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.drawernav);
         getFragment(new home());
 
 
+        nav_view = findViewById(R.id.drawerview);
+        dlay = findViewById(R.id.drawernav);
+        toggle =new ActionBarDrawerToggle(this, dlay, R.string.Open, R.string.Close);
+        dlay.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.Akunsaya){
+                    getSupportActionBar().setTitle("Akun saya");
+                    getFragment(new akun_saya());
+                }else if(item.getItemId() == R.id.Bantuan) {
+                    getSupportActionBar().setTitle("Bantuan");
+                    getFragment(new bantuan());
+                }
+                dlay.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
         btm_view = findViewById(R.id.bottom_view);
         btm_view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -45,10 +72,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
     private void getFragment(Fragment fragment) {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = manager.beginTransaction();
         fragmentTransaction.replace(R.id.isi,fragment);
         fragmentTransaction.commit();
     }
+
+
 }
