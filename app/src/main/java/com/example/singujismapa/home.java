501807c11b6308309aeb8a86c8bb18cbc7322c38
ummalatulkamilah <55658527+ummalatulkamilah@users.noreply.github.com>
2020-06-26 11:30:09 +0700

    tambah home.java
    volley dan json untuk home diatur disini

--- app/src/main/java/com/example/singujismapa/home.java
+++ app/src/main/java/com/example/singujismapa/home.java
@@ -3,10 +3,27 @@ package com.example.singujismapa;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +31,6 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class home extends Fragment {
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,9 +40,7 @@ public class home extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public home() {
-        // Required empty public constructor
-    }
+
 
     /**
      * Use this factory method to create a new instance of
@@ -55,10 +69,64 @@ public class home extends Fragment {
         }
     }
 
+    private RecyclerView recyclerView;
+    private HomeAdapter adapter;
+    private List<HomeItem> homeitem;
+    private final String URL_TASK =  "http://192.168.43.219/SingujiSmapa/folder_php/home.php";
+
+    public home() {
+        // Required empty public constructor
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.list_data);
+        homeitem = new ArrayList<>();
+        extractHome();
+
+        return view;
     }
+
+    private void extractHome()
+    {
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET, URL_TASK, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        for (int i = 0; i < response.length(); i++)
+                        {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                HomeItem md = new HomeItem();
+                                md.setNamaData(jsonObject.getString("mata_pelajaran"));
+                                md.setNamaGuru(jsonObject.getString("nama_guru"));
+
+                                homeitem.add(md);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(getActivity().getApplicationContext(), "Error" + e.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+                        adapter = new HomeAdapter(getActivity().getApplicationContext(), homeitem);
+                        recyclerView.setAdapter(adapter);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Error" + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        requestQueue.add(jsonRequest);
+    }
+
 }
