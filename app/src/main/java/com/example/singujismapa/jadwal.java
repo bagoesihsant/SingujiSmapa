6a2666560663e94c2d10521147a2e22ba7d7ecd3
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-07-02 13:00:26 +0700

    Menambahkan setToken untuk file yang berasal dari JSON

--- app/src/main/java/com/example/singujismapa/jadwal.java
+++ app/src/main/java/com/example/singujismapa/jadwal.java
@@ -76,7 +76,7 @@ public class jadwal extends Fragment {
         }
     }
     //URL JANGAN LUPA DIGANTI INI TEMAN TEMAN KALO MAU NGE RUN
-    private final String URL_Jadwal = "http://192.168.1.20/SingujiSmapa/folder_php/jadwal.php";
+    private final String URL_Jadwal = "http://192.168.43.132/SingujiSmapa/folder_php/jadwal.php";
     private RecyclerView myrecyclerview ;
     private List<JadwalItem> lstjadwal;
     private RecyclerViewAdapter_jadwal adapter;
@@ -121,6 +121,7 @@ public class jadwal extends Fragment {
                                 jadwal.setStatussoal(jsonObject.getString("tipe_ujian"));
                                 jadwal.setJumlahsoal(jsonObject.getString("jumlah_soal"));
                                 jadwal.setWaktumulai(jsonObject.getString("waktu_mengerjakan"));
+                                jadwal.setTokenSoal(jsonObject.getString("token_soal"));
                                 lstjadwal.add(jadwal);
                             } catch (JSONException e) {
                                 e.printStackTrace();
